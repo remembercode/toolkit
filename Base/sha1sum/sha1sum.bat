337543a269b3a5e3ab2bbf16d38ac3549f2a6639
Wang Bing <wangbing1985@163.com>
2017-01-05 16:57:37 +0800

    add Base/sha1sum to repo

---
+++ Base/sha1sum/sha1sum.bat
@@ -0,0 +1,38 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+if not exist "%~1" (
+	echo Error : "%~1" doesn`t exist!
+	goto :eof
+)
+
+set inputted_sha1=%~2
+if exist "!inputted_sha1!" (
+	for /f "delims=" %%i in (!inputted_sha1!) do (
+		set inputted_sha1=%%i
+		REM echo %%i
+		goto read_sha1_from_file
+	)
+)
+:read_sha1_from_file
+
+set inputted_sha1=!inputted_sha1:A=a!
+set inputted_sha1=!inputted_sha1:B=b!
+set inputted_sha1=!inputted_sha1:C=c!
+set inputted_sha1=!inputted_sha1:D=d!
+set inputted_sha1=!inputted_sha1:E=e!
+set inputted_sha1=!inputted_sha1:F=f!
+
+for /f "tokens=1-20 skip=1 delims= " %%a in ('certutil -hashfile "%~1" sha1') do (
+	set computed_sha1=%%a%%b%%c%%d%%e%%f%%g%%h%%i%%j%%k%%l%%m%%n%%o%%p%%q%%r%%s%%t
+	if "!computed_sha1!" EQU "!inputted_sha1!" (
+		echo Success
+	) else (
+		echo Computed SHA1 : !computed_sha1!
+		echo Inputted SHA1 : !inputted_sha1!
+		echo Fail
+	)
+	goto end
+)
+:end
+goto :eof
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 plain text develop resources base guide
 
 [md5sum](/Base/md5sum)
+[sha1sum](/Base/sha1sum)
